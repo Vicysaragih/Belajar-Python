de5d1290674ee768411574849179721a0e7bd7fe
Vicy A C Saragih <101926088+Vicysaragih@users.noreply.github.com>
2022-10-11 16:53:50 +0700

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,20 @@
+# Mengubah Tipe Data Float menjadi Integer
+from tkinter import Y
+
+
+print(int(1.3))
+
+# Mengubah Tipe Data Integer MEnjadi Float
+print(float(2))
+
+# Boolean
+print(type(True))
+
+print(5*5)
+
+x=20
+y=x/60
+print(Y)
+
+print(3 * "Vicy")
+
