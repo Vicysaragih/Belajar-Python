72a51a04893f2cd0a230239b19d5d94dad12099d
Vicy A C Saragih <101926088+Vicysaragih@users.noreply.github.com>
2022-10-18 16:44:54 +0700

    Add files via upload

---
+++ Operasi_List.py
@@ -0,0 +1,19 @@
+Contoh_angka = [2,4,5,8,4,6,3,9,2,4,7,5,8,]
+print(f" Kumpulan angka\t: {Contoh_angka}")
+print(Contoh_angka)
+
+angka_baru = Contoh_angka.count(4)
+angka_baru8 = Contoh_angka.count(8)
+
+print(f"Jumlah angka 4 :{angka_baru}")
+print(f"jumlah angka 8 : {angka_baru8}")
+
+# Mencari Index data 
+data = ["batak", "ambon", "jawa"]
+
+index_data = data.index("batak")
+print(f"Daftar :{index_data}")
+
+# Mengurutkan List data
+Contoh_angka.sort()
+print(Contoh_angka)
